eb00f2f02005449f7863d646e61efae0c1abffbc
zellhuang0503 <zell.huang@gmail.com>
2026-01-16 18:56:09 +0800

    Update About schema, add media plugin and dashboard widgets

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
+import { media } from 'sanity-plugin-media'
+import { dashboardTool, projectUsersWidget, projectInfoWidget } from '@sanity/dashboard'
+import { documentListWidget } from 'sanity-plugin-dashboard-widget-document-list'
 import { schemaTypes } from './schemas'
 
 export default defineConfig({
@@ -10,7 +13,23 @@ export default defineConfig({
     projectId: 'vm3p10fe',
     dataset: 'production',
 
-    plugins: [structureTool(), visionTool()],
+    plugins: [
+        structureTool(),
+        visionTool(),
+        media(),
+        dashboardTool({
+            widgets: [
+                documentListWidget({
+                    title: '最近編輯 (Recent Edits)',
+                    order: '_updatedAt desc',
+                    limit: 10,
+                    types: ['about', 'village', 'story'] // customize based on your schema types
+                }),
+                projectInfoWidget(),
+                projectUsersWidget()
+            ]
+        })
+    ],
 
     schema: {
         types: schemaTypes,
